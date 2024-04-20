38da27636f7457516b624fbae93f223e1cd8d212
Sergei Fedorov <imfedr@gmail.com>
2024-04-20 22:38:43 +0200

    Add npm install

--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -13,9 +13,11 @@ while true; do
     echo "No changes found."
   else
     echo "Changes found. Deploying the changes..."
+    git clean -df || { echo "Failed to clean the repository"; exit 1; }
     git pull || { echo "Failed to pull from remote repository"; exit 1; }
     sudo systemctl restart nginx || { echo "Failed to restart nginx"; exit 1; }
-    sudo systemctl restart ws-server || { echo "Failed to restart nginx"; exit 1; }
+    (cd backend && npm install) || { echo "Failed to install npm packages"; exit 1; }
+    sudo systemctl restart ws-server || { echo "Failed to restart ws-server"; exit 1; }
     echo "Deployment completed."
   fi
 
--- cicd/setup.sh
+++ cicd/setup.sh
@@ -12,18 +12,18 @@ sudo apt install -y nginx nodejs npm
 cd ~
 git clone https://github.com/fed-off/board-games.git repo
 cd repo
-npm install
-
-# Configure Node.js app as a service (backend)
-sudo cp cicd/ws-server.service /etc/systemd/system/
-sudo systemctl daemon-reload
-sudo systemctl start ws-server
-sudo systemctl enable ws-server
 
 # Configure Nginx (frontend)
 sudo usermod -a -G ubuntu www-data
 sudo cp cicd/nginx.conf /etc/nginx/sites-available/default
 sudo systemctl restart nginx
+
+# Configure Node.js app as a service (backend)
+(cd backend && npm install)
+sudo cp cicd/ws-server.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl start ws-server
+sudo systemctl enable ws-server
 
 # Configure continuous deployment
 sudo cp cicd/deploy.service /etc/systemd/system/
